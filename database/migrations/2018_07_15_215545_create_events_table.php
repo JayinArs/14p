bb8519b60bde73d2e621e0686ea97215de1cce7c
AbydArs <abidr.w@gmail.com>
2018-07-18 23:39:35 +0500

    alter date to string

--- database/migrations/2018_07_15_215545_create_events_table.php
+++ database/migrations/2018_07_15_215545_create_events_table.php
@@ -16,7 +16,7 @@ class CreateEventsTable extends Migration
 		Schema::create( 'events', function ( Blueprint $table ) {
 			$table->increments( 'id' );
 			$table->string( 'title' );
-			$table->date( 'hijri_date' )->nullable();
+			$table->string( 'hijri_date' )->nullable();
 		} );
 	}
 
